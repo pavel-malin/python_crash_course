e991b3d88bfd255af1e2f6ea1165e9f99f26d628
pavel-malin <kurchevskijpavel@gmail.com>
2019-11-03 17:23:35 +0300

    new pep8

--- python_game/scoreboard.py
+++ python_game/scoreboard.py
@@ -72,5 +72,6 @@ class Scoreboard():
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        
         # Output ship
         self.ships.draw(self.screen)
--- python_game/settings.py
+++ python_game/settings.py
@@ -3,6 +3,7 @@ class Settings():
 
     def __init__(self):
         """Initialize the game's settings."""
+
         # Screen settings.
         self.screen_width = 1200
         self.screen_height = 640
@@ -22,11 +23,13 @@ class Settings():
         # Alien setting.
         self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
+
         # fleet_direction of 1 represents right, -1 represents left.
         self.fleet_direction = 1
 
         # Game acceleration rate
         self.speedup_scale = 1.1
+
         # The growth rate of the cost of aliens.
         self.score_scale = 1.5
         self.initialize_dynamic_settings()
@@ -48,5 +51,4 @@ class Settings():
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
-
         self.alien_points = int(self.alien_points * self.score_scale)
